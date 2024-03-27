20237736856bec494e653cdc8ceabf9ef3ef76d6
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-03-27 23:11:00 +0000

    Update plus.sh

--- plus.sh
+++ plus.sh
@@ -1,1 +1,1 @@
-paru -S cowsay ponysay doas gnome-extra fcitx5-mozc adobe-source-han-sans-fonts adobe-source-han-serif-fonts
+paru -S cowsay ponysay doas gnome-extra fcitx5-mozc adobe-source-han-sans-fonts adobe-source-han-serif-fonts tumux
