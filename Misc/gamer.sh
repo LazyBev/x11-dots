9204d541240eebeadfdaf6f115575e09067823d7
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-03-27 05:32:37 +0000

    Update gamer.sh

--- Misc/gamer.sh
+++ Misc/gamer.sh
@@ -1,1 +1,1 @@
-sudo pacman 
+paru -S vesktop-bin steam opentabletdriver wine && git clone https://github.com/NelloKudo/osu-winello.git && cd osu-winello && chmod +x osu-winello.sh && ./osu-winello.sh
