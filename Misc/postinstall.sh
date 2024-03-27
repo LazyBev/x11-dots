055caaeabfe9fbff812beae0e5301e0b4fa06983
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-03-27 21:42:55 +0000

    Update postinstall.sh

--- Misc/postinstall.sh
+++ Misc/postinstall.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 sudo pacman -Syu 
-sudo mkdir ~/.local/share/fonts 
 sudo mkdir Pictures 
 sudo mkdir Videos 
 sudo setxkbmap -layout gb 
@@ -16,7 +15,8 @@ sudo rm -rf fish
 sudo rm -rf nvim 
 sudo rm -rf /etc/pacman.conf 
 sudo rm -rf /etc/xdg/picom.conf 
-sudo rm -rf omf  
+sudo rm -rf omf 
+sudo ln -s ~/dotfiles/fonts ~/.local/share
 sudo ln -s ~/dotfiles/omf  
 sudo ln -s ~/dotfiles/fish  
 sudo ln -s ~/dotfiles/i3  
@@ -25,8 +25,8 @@ sudo ln -s ~/dotfiles/rofi
 sudo ln -s ~/dotfiles/Misc/pacman.conf /etc
 sudo ln -s ~/dotfiles/Misc/picom.conf /etc/xdg  
 sudo chsh $USER -s /bin/fish  
-read -p "Please make sure to run 'sudo nano /etc/doas.conf' then write 'permit (whatever your username is)' then save and quit...(Press enter to continue)"  
-gitdot  
+gitdot 
+cd ~
 clear  
-cd ~  
-read -p "Press enter in order for changes to take place..."  sudo reboot
+read -p "Press enter in order for changes to take place..."  
+sudo reboot
