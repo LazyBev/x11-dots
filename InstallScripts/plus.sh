6de810bee160d96ab3f390cd5fd7591a7c34f3c8
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-05 07:51:36 +0000

    Update plus.sh

--- InstallScripts/plus.sh
+++ InstallScripts/plus.sh
@@ -1,2 +1,2 @@
-paru -S cowsay ponysay doas gnome-extra fcitx5-mozc adobe-source-han-sans-fonts adobe-source-han-serif-fonts tumux virt-manager qemu dnsmasq libvirt
+paru -S cowsay ponysay doas gnome-extra tumux virt-manager qemu dnsmasq libvirt
 sudo usermod -aG libvirt $USER
