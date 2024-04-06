9f672e6a6a56d7c8ea688941dafe0ca444bbfd24
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-06 17:50:06 +0100

    Update gaming.sh

--- InstallScripts/gaming.sh
+++ InstallScripts/gaming.sh
@@ -1,4 +1,5 @@
 yay -S vesktop-bin steam wine 
+cd ~
 git clone https://github.com/NelloKudo/osu-winello.git  
 cd osu-winello  
 chmod +x osu-winello.sh  
@@ -7,6 +8,5 @@ git clone https://aur.archlinux.org/opentabletdriver.git
 cd opentabletdriver  
 makepkg -si  
 cd ~ 
-sudo rm -rf opentabletdriver  
 sudo mkinitcpio -P  
 sudo rmmod wacom hid_uclogic
