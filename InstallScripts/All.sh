14280f0d23f8fa9fc214571c3a995a5438bf329b
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-07 09:40:10 +0100

    Update All.sh

--- InstallScripts/All.sh
+++ InstallScripts/All.sh
@@ -1,7 +1,5 @@
 cd ~/dotfiles/InstallScripts
-sudo chmod +x install.sh
-sudo chmod +x plus.sh
+sudo chmod +x lazyinstall.sh
 sudo chmod +x gaming.sh
-./install.sh
-./plus.sh
+./lazyinstall.sh
 ./gaming.sh
