6ad5398100395fb625ce671238cfc54a8d95b45c
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-07 01:26:42 +0100

    Update install.sh

--- InstallScripts/install.sh
+++ InstallScripts/install.sh
@@ -2,15 +2,24 @@
 
 sudo pacman -Syu 
 cd ~
-sudo mkdir Pictures 
-sudo mkdir Videos 
+if [ -d ~/Pictures]; then
+  echo Pictures dir exists
+else
+  sudo mkdir Pictures
+fi
+if [ -d ~/Videos]; then
+  echo Videos dir exists
+else
+  sudo mkdir Videos
+fi
 sudo setxkbmap -layout gb 
 sudo git clone https://aur.archlinux.org/yay-bin.git 
 cd ~/yay-bin
 makepkg -si 
 cd ~ 
-yay -S man i3 vesktop-bin wine mercury-browser-bin flameshot picom zip unzip fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts neofetch fcitx5-im lolcat polkit gvfs alsa-utils pipewire pipewire-pulse pavucontrol dunst xarchiver thunar thunar-archive-plugin lxappearance eza rofi fish bottom neovim nitrogen alacritty 
+yay -S man i3 mercury-browser-bin flameshot picom zip unzip neofetch lolcat polkit gvfs alsa-utils pipewire pipewire-pulse pavucontrol dunst xarchiver thunar thunar-archive-plugin lxappearance eza rofi fish bottom neovim nitrogen alacritty 
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+fish
 cd ~/.config 
 sudo rm -rf i3 
 sudo rm -rf rofi
@@ -36,7 +45,8 @@ sudo ln -s ~/dotfiles/nvim
 sudo ln -s ~/dotfiles/rofi  
 sudo ln -s ~/dotfiles/pacman.conf /etc
 sudo ln -s ~/dotfiles/picom.conf /etc/xdg  
-sudo chsh $USER -s /bin/fish  
-gitdot 
 cd ~
-read -p "Reboot in order for changes to take place..."  
+sudo chsh $USER -s /bin/fish  
+gitdot
+read -p "Rebooting in order for changes to take place..."  
+sudo reboot
