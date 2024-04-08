cf503db0a91cec2e185a897111a65b9296ff4dda
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-09 00:39:02 +0100

    Update liteinstall.sh

--- liteinstall.sh
+++ liteinstall.sh
@@ -7,12 +7,11 @@ if [ YN == "no" ]; then
     makepkg -sci
     cd ~
 fi
-yay -S man curl wget vim neovim nitrogen mercury-browser-bin flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom vesktop-bin wine-staging fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im steam
+yay -S man curl wget vim neovim nitrogen mercury-browser-bin flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom vesktop-bin wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im steam
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 chsh $USER -s /bin/fish
 cd ~/dotfiles
 sudo cp -rp fcitx5 ../.config
-sudo cp -rp fcitx ../.config
 sudo cp -rp mozc ../.config
 sudo cp -rp fonts ~/.local/share
 sudo cp -rp fish ../.config
