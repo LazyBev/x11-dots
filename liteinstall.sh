f691d6e1c2c10ce5e3627409a4b7af6133ff7434
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-08 18:48:01 +0100

    Update liteinstall.sh

--- liteinstall.sh
+++ liteinstall.sh
@@ -9,17 +9,17 @@ if [ YN == "no" ]; then
 fi
 yay -S man curl wget vim neovim nitrogen mercury-browser-bin flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom vesktop-bin wine-staging fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im steam
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-cd ~/.config 
-sudo cp -rp ~/dotfiles/fcitx5
-sudo cp -rp ~/dotfiles/fcitx
-sudo cp -rp ~/dotfiles/mozc 
-sudo cp -rp ~/dotfiles/fonts ~/.local/share
-sudo cp -rp ~/dotfiles/omf  
-sudo cp -rp ~/dotfiles/fish  
-sudo cp -rp ~/dotfiles/i3  
-sudo cp -rp ~/dotfiles/nvim  
-sudo cp -rp ~/dotfiles/rofi  
-sudo cp -rp ~/dotfiles/picom.conf
-sudo cp -rp ~/dotfiles/neofetch 
-sudo cp -rp ~/dotfiles/pacman.conf /etc
+chsh $USER -s /bin/fish
+cd ~/dotfiles
+sudo cp -rp fcitx5 ../.config
+sudo cp -rp fcitx ../.config
+sudo cp -rp mozc ../.config
+sudo cp -rp fonts ~/.local/share
+sudo cp -rp omf ../.config
+sudo cp -rp fish ../.config
+sudo cp -rp i3 ../.config
+sudo cp -rp nvim ../.config
+sudo cp -rp rofi ../.config
+sudo cp -rp picom.conf ../.config
+sudo cp -rp pacman.conf /etc
 cd ~
