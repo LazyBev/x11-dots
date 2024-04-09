4afbdd3cd067afe2f7bd6077e331034e580fe11e
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-09 23:48:59 +0000

    Update liteinstall.sh

--- liteinstall.sh
+++ liteinstall.sh
@@ -1,16 +1,6 @@
-echo "Are you using yay?"
-read YN
-if [ YN == "no" ]; then
-    cd ~
-    sudo git clone https://aur.archlinux.org/yay-bin.git 
-    cd yay-bin
-    makepkg -sci
-    cd ~
-fi
-yay -S man curl wget vim neovim nitrogen mercury-browser-bin flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom vesktop-bin wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im steam
+yay -S man curl wget vim neovim nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im 
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 chsh $USER -s /bin/fish
-cd ~/dotfiles
 sudo cp -rp fcitx5 ../.config
 sudo cp -rp mozc ../.config
 sudo cp -rp fonts ~/.local/share
@@ -20,4 +10,3 @@ sudo cp -rp nvim ../.config
 sudo cp -rp rofi ../.config
 sudo cp -rp picom.conf ../.config
 sudo cp -rp pacman.conf /etc
-cd ~
