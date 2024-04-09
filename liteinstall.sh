63b272f0e0c63889e78c30619ca2bab43e685127
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-09 23:49:18 +0000

    Update liteinstall.sh

--- liteinstall.sh
+++ liteinstall.sh
@@ -1,4 +1,4 @@
-yay -S man curl wget vim neovim nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im 
+sudo pacman -S man curl wget vim neovim nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im 
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 chsh $USER -s /bin/fish
 sudo cp -rp fcitx5 ../.config
