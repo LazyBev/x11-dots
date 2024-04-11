ff46253c8406f6f9095475d6fdd312156246672d
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-11 07:17:57 +0100

    Update postarchinstall.sh

--- postarchinstall.sh
+++ postarchinstall.sh
@@ -6,7 +6,10 @@ if [ YN == "no" ] then
     makepkg -sci
     cd ../dotfiles
 fi
-yay -S man vesktop-bin curl wget vim neovim nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im 
+read -p "Do you want a minimal config?" YN
+if [ YN == "no" ] then
+    yay -S man vesktop-bin curl wget vim neovim nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im 
+fi
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 sudo cp -rp fcitx5 ../.config
 sudo cp -rp mozc ../.config
@@ -19,3 +22,4 @@ sudo cp -rp picom.conf ../.config
 sudo cp -rp pacman.conf /etc
 # Custom command i added to fish config
 tofish
+links https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.29/bedrock-linux-0.7.29-x86_64.sh
