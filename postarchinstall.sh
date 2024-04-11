1cd7ec4b89b87026edd9ab343e3b1c8d0b261544
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-11 07:19:00 +0100

    Update postarchinstall.sh

--- postarchinstall.sh
+++ postarchinstall.sh
@@ -1,5 +1,5 @@
 read -p "Do you have yay installed?" YN
-if [ YN == "no" ] then
+if [ YN == "no" ]; then
     cd ..
     sudo git clone https://aur.archlinux.org/yay-bin.git 
     cd yay-bin
@@ -7,7 +7,7 @@ if [ YN == "no" ] then
     cd ../dotfiles
 fi
 read -p "Do you want a minimal config?" YN
-if [ YN == "no" ] then
+if [ YN == "no" ]; then
     yay -S man vesktop-bin curl wget vim neovim nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im 
 fi
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
