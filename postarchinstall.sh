1b4810a57dea5639a65616c30bd4233914505100
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-11 08:09:38 +0100

    Update postarchinstall.sh

--- postarchinstall.sh
+++ postarchinstall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash 
+
 cd ..
 sudo rm -rf yay-bin
 sudo git clone https://aur.archlinux.org/yay-bin.git 
 cd yay-bin
 makepkg -sci
 cd ../dotfiles
-# yay -S man vesktop-bin curl wget vim neovim nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im 
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+# yay -S man vesktop-bin curl wget vim neovim nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im fish
 sudo cp -rp fcitx5 ../.config
 sudo cp -rp mozc ../.config
 sudo cp -rp fonts ~/.local/share
