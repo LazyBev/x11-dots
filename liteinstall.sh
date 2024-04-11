196a638539d25519c28d421de480a28298d0f683
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-11 05:01:00 +0100

    Update liteinstall.sh

--- liteinstall.sh
+++ liteinstall.sh
@@ -8,10 +8,6 @@ if [ YN == "no" ] then
 fi
 yay -S man vesktop-bin steam curl wget vim neovim nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im 
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-which $SHELL
-if [[ which $SHELL == "/bin/fish" ]] then
-    chsh $USER -s /bin/fish
-fi
 sudo cp -rp fcitx5 ../.config
 sudo cp -rp mozc ../.config
 sudo cp -rp fonts ~/.local/share
@@ -21,3 +17,5 @@ sudo cp -rp nvim ../.config
 sudo cp -rp rofi ../.config
 sudo cp -rp picom.conf ../.config
 sudo cp -rp pacman.conf /etc
+# Custom command i added to fish config
+tofish
