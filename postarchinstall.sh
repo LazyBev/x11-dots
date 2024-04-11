1478f84b977a831fb5688749b5034c66109c3053
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-11 07:24:19 +0100

    Update postarchinstall.sh

--- postarchinstall.sh
+++ postarchinstall.sh
@@ -1,17 +1,10 @@
-#!/bin/bash
-
-read -p "Do you have yay installed?" YN
-if [ YN == "no" ]; then
-    cd ..
-    sudo git clone https://aur.archlinux.org/yay-bin.git 
-    cd yay-bin
-    makepkg -sci
-    cd ../dotfiles
-fi
-read -p "Do you want a minimal config?" YN
-if [ YN == "no" ]; then
-    yay -S man vesktop-bin curl wget vim neovim nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im 
-fi
+#!/bin/bash 
+cd ..
+sudo git clone https://aur.archlinux.org/yay-bin.git 
+cd yay-bin
+makepkg -sci
+cd ../dotfiles
+# yay -S man vesktop-bin curl wget vim neovim nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im 
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 sudo cp -rp fcitx5 ../.config
 sudo cp -rp mozc ../.config
