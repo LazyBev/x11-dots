c92858f270f91b84000484eeee10baf7cb6a5e28
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-11 05:07:14 +0000

    Update postarchinstall.sh

--- postarchinstall.sh
+++ postarchinstall.sh
@@ -6,7 +6,7 @@ if [ YN == "no" ] then
     makepkg -sci
     cd ../dotfiles
 fi
-yay -S man vesktop-bin steam curl wget vim neovim nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im 
+yay -S man vesktop-bin curl wget vim neovim nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im 
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 sudo cp -rp fcitx5 ../.config
 sudo cp -rp mozc ../.config
