dcc160424c2b3b8df9a18c85c5438e1e5705df0c
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-11 08:16:01 +0100

    Update postarchinstall.sh

--- postarchinstall.sh
+++ postarchinstall.sh
@@ -1,12 +1,13 @@
 #!/bin/bash 
 
 cd ..
-sudo rm -rf yay-bin
-sudo git clone https://aur.archlinux.org/yay-bin.git 
-cd yay-bin
+sudo rm -rf paru
+sudo git clone https://aur.archlinux.org/paru.git 
+cd paru
 makepkg -sci
 cd ../dotfiles
-# yay -S man vesktop-bin curl wget vim neovim nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im fish
+yay -S man vesktop-bin curl wget vim neovim nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 sudo cp -rp fcitx5 ../.config
 sudo cp -rp mozc ../.config
 sudo cp -rp fonts ~/.local/share
@@ -16,5 +17,6 @@ sudo cp -rp nvim ../.config
 sudo cp -rp rofi ../.config
 sudo cp -rp picom.conf ../.config
 sudo cp -rp pacman.conf /etc
+fish
 # Custom command i added to fish config
 tofish
