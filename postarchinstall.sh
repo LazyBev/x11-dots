fc9378dbfba99c046184677d1e79d150a2a01708
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-12 02:48:50 +0100

    Update postarchinstall.sh

--- postarchinstall.sh
+++ postarchinstall.sh
@@ -7,7 +7,6 @@ cd paru
 makepkg -sci
 cd ../dotfiles
 paru -S man vesktop-bin curl wget vim neovim nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber lolcat gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im fish
-curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 sudo cp -rp fcitx5 ../.config
 sudo cp -rp mozc ../.config
 sudo cp -rp fonts ~/.local/share
@@ -17,6 +16,6 @@ sudo cp -rp nvim ../.config
 sudo cp -rp rofi ../.config
 sudo cp -rp picom.conf ../.config
 sudo cp -rp pacman.conf /etc
-fish
-# Custom command i added to fish config
-tofish
+sh <(curl -L https://nixos.org/nix/install) --daemon
+https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.29/bedrock-linux-0.7.29-x86_64.sh
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
