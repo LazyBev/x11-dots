bf792885b724441259d04e3869203638673634a9
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-14 01:29:31 +0100

    Create UpdateConfig.sh

---
+++ UpdateConfig.sh
@@ -0,0 +1,9 @@
+sudo cp -rp fcitx5 ../.config
+sudo cp -rp mozc ../.config
+sudo cp -rp fonts ~/.local/share
+sudo cp -rp fish ../.config
+sudo cp -rp i3 ../.config
+sudo cp -rp nvim ../.config
+sudo cp -rp rofi ../.config
+sudo cp -rp picom.conf ../.config
+sudo cp -rp pacman.conf /etc
