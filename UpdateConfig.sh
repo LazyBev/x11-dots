98bf4a0c622230a17d786ef5f07a839fb8de9747
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-14 18:42:20 +0100

    Update UpdateConfig.sh

--- UpdateConfig.sh
+++ UpdateConfig.sh
@@ -1,11 +1,19 @@
-sudo cp -rp fcitx5 ../.config
-sudo cp -rp mozc ../.config
-sudo cp -rp fonts ~/.local/share
-sudo cp -rp fish ../.config
-sudo cp -rp i3 ../.config
-sudo cp -rp nvim ../.config
-sudo cp -rp rofi ../.config
-sudo cp -rp picom.conf ../.config
-sudo cp -rp pacman.conf /etc
+#!/bin/bash
+
+echo "---- Making backup at ~/configBackup -----"
+cp -rpf ../.config ../configBackup 
+echo "----- Backup made at ~/configBackup ------"
+
+sudo cp -rpf Pictures/bgpic.jpg
+sudo cp -rpf nitrogen ../.config
+sudo cp -rpf fcitx5 ../.config
+sudo cp -rpf mozc ../.config
+sudo cp -rpf fonts ~/.local/share
+sudo cp -rpf fish ../.config
+sudo cp -rpf i3 ../.config
+sudo cp -rpf nvim ../.config
+sudo cp -rpf rofi ../.config
+sudo cp -rpf picom.conf ../.config
+sudo cp -rpf pacman.conf /etc
 
 echo "Press Mod + Shift + c to refresh i3 config"
