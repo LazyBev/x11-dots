73e59b7e5b6e79e6d5d91926ab89d57d5a9e0010
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-14 18:37:27 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -11,18 +11,22 @@ if [ $YN == "no" ]; then
   cd ../dotfiles
 fi
 
-paru -S man xdotool vesktop-bin curl rofi mercury-browser-bin wget vim neovim neofetch lolcat nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-cn-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-jp-fonts fish
+paru -S man xdotool vesktop-bin curl rofi mercury-browser-bin wget vim neovim neofetch nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-cn-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-jp-fonts fish
 
-sudo cp -rp nitrogen ../.config
-sudo cp -rp fcitx5 ../.config
-sudo cp -rp mozc ../.config
-sudo cp -rp fonts ~/.local/share
-sudo cp -rp fish ../.config
-sudo cp -rp i3 ../.config
-sudo cp -rp nvim ../.config
-sudo cp -rp rofi ../.config
-sudo cp -rp picom.conf ../.config
-sudo cp -rp pacman.conf /etc
+echo "---- Making backup at ~/configBackup -----"
+cp -rpf ../.config ../configBackup 
+echo "----- Backup made at ~/configBackup ------"
+
+sudo cp -rpf nitrogen ../.config
+sudo cp -rpf fcitx5 ../.config
+sudo cp -rpf mozc ../.config
+sudo cp -rpf fonts ~/.local/share
+sudo cp -rpf fish ../.config
+sudo cp -rpf i3 ../.config
+sudo cp -rpf nvim ../.config
+sudo cp -rpf rofi ../.config
+sudo cp -rpf picom.conf ../.config
+sudo cp -rpf pacman.conf /etc
 
 cd ~
 if [[ -d ~/Pictures ]]; then
