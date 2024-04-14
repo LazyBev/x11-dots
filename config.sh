79434b90b775e996c82a61a8a7e85a662f55e62b
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-14 07:05:01 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -2,7 +2,7 @@
 set -e
 
 read -p "Do you have paru installed?" YN
-if [ $YN == "no" ]; then 
+if [ $YN == "no" ]; then
   cd ..
   git clone "https://aur.archlinux.org/paru.git"
   sudo chown $USER:$USER -R ~
@@ -10,7 +10,9 @@ if [ $YN == "no" ]; then
   makepkg -sci
   cd ../dotfiles
 fi
-paru -S man xdotool vesktop-bin curl rofi mercury-browser-bin wget vim neovim neofetch lolcat nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts fcitx5-im fish
+
+paru -S man xdotool vesktop-bin curl rofi mercury-browser-bin wget vim neovim neofetch lolcat nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber gvfs dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-cn-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-jp-fonts fish
+
 sudo cp -rp fcitx5 ../.config
 sudo cp -rp mozc ../.config
 sudo cp -rp fonts ~/.local/share
@@ -20,5 +22,19 @@ sudo cp -rp nvim ../.config
 sudo cp -rp rofi ../.config
 sudo cp -rp picom.conf ../.config
 sudo cp -rp pacman.conf /etc
-https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.29/bedrock-linux-0.7.29-x86_64.sh 
+read -p "Do you want bedrock linux?" YN
+
+if [ $YN == "yes" ]; then
+  curl -LO https://github.com/bedrocklinux/bedrocklinux-userland/releases/download/0.7.29/bedrock-linux-0.7.29-x86_64.sh
+  chmod +x bedrock-linux-0.7.29-x86_64.sh
+  ./bedrock-linux-0.7.29-x86_64.sh
+fi
+
+bash <<"EOF"
+read -p "Do you want nix package manager" YN
+if [ $YN == "yes" ]; then
+  sh <(curl -L https://nixos.org/nix/install) --daemon
+fi
+exit
+EOF
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
