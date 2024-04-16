bdb259313798d675b2db7b565f81294826a44f3e
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-04-16 02:53:54 +0100

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,20 +1,21 @@
 #!/bin/bash 
+
 set -e
 
 read -p "Do you have paru installed? " YN
 if [ $YN == "no" ]; then
-  cd ..
+  cd ~
   git clone "https://aur.archlinux.org/paru.git"
   sudo chown $USER:$USER -R ~
   cd paru 
   makepkg -sci
-  cd ../dotfiles
+  cd ~/dotfiles
 fi
 
-paru -S steam obsidian man xdotool vesktop-bin curl rofi mercury-browser-bin wget vim neovim neofetch nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber gvfs polkit-gnome dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-cn-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-jp-fonts fish
+paru -S btop steam obsidian man xdotool vesktop-bin curl rofi mercury-browser-bin wget vim neovim neofetch nitrogen flameshot zip unzip mpv cmake alacritty picom wireplumber gvfs polkit-gnome dunst xarchiver thunar thunar-archive-plugin lxappearance eza fish bottom wine-staging fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-cn-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-jp-fonts fish
 
 echo "---- Making backup at ~/configBackup -----"
-cp -rpf ~/.config ~/configBackup 
+sudo cp -rpf ~/.config ~/configBackup 
 echo "----- Backup made at ~/configBackup ------"
 
 sudo cp -rpf ~/dotfiles/tmux ~/.config
@@ -41,7 +42,7 @@ else
 fi
 
 if [[ -d ~/Videos ]]; then
-    echo "Videos dir exists" 
+    echo "Videos dir exists"
 else
     sudo cp -rpf Videos ~
     sudo rm -rf ~/Videos/
@@ -50,4 +51,11 @@ fi
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
 alacritty -e fish | tofish
 
-echo "Press Mod + Shift + c to refresh i3 config, also reboot"
+paru -S nvidia-lts linux-lts linux-lts-headers
+paru -R linux linux-headers
+
+sudo cp -rpf ~/dotfiles/mkinitcpio.conf /etc/
+sudo mkinitcpio -P
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+reboot
