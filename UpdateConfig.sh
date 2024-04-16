c2cd7bf7e77fcee288a62331517ed14c1b7bbcbd
LazyBev <orangekirea@gmail.com>
2024-04-16 01:34:30 +0100

    updated

--- UpdateConfig.sh
+++ UpdateConfig.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 echo "---- Making backup at ~/configBackup -----"
-sudo cp -rpf ../.config ../configBackup 
+sudo cp -rpf ~/.config ~/configBackup 
 echo "----- Backup made at ~/configBackup ------"
 
-sudo cp -rpf tmux ../.config
-sudo cp -rpf dunst ../.config
-sudo cp -rpf alacritty ../.config
-sudo cp -rpf Pictures/bgpic.jpg ../Pictures
-sudo cp -rpf nitrogen ../.config
-sudo cp -rpf fcitx5 ../.config
-sudo cp -rpf mozc ../.config
-sudo cp -rpf fonts/fontconfig ../.config
-sudo cp -rpf fonts/MartianMono ~/.local/share/fonts
-sudo cp -rpf fonts/SF-Mono-Powerline ~/.local/share/fonts
-sudo cp -rpf fish ../.config
-sudo cp -rpf i3 ../.config
-sudo cp -rpf nvim ../.config
-sudo cp -rpf rofi ../.config
-sudo cp -rpf omf ../.config
-sudo cp -rpf picom.conf ../.config
-sudo cp -rpf pacman.conf /etc
+sudo cp -rpf ~/dotfiles/tmux ~/.config
+sudo cp -rpf ~/dotfiles/dunst ~/.config
+sudo cp -rpf ~/dotfiles/alacritty ~/.config
+sudo cp -rpf ~/dotfiles/Pictures/bgpic.jpg ../Pictures
+sudo cp -rpf ~/dotfiles/nitrogen ~/.config
+sudo cp -rpf ~/dotfiles/fcitx5 ~/.config
+sudo cp -rpf ~/dotfiles/ mozc ~/.config
+sudo cp -rpf ~/dotfiles/fonts/fontconfig ~/.config
+sudo cp -rpf ~/dotfiles/fonts/MartianMono ~/.local/share/fonts
+sudo cp -rpf ~/dotfiles/fonts/SF-Mono-Powerline ~/.local/share/fonts
+sudo cp -rpf ~/dotfiles/fish ~/.config
+sudo cp -rpf ~/dotfiles/i3 ~/.config
+sudo cp -rpf ~/dotfiles/nvim ~/.config
+sudo cp -rpf ~/dotfiles/rofi ~/.config
+sudo cp -rpf ~/dotfiles/omf ~/.config
+sudo cp -rpf ~/dotfiles/picom.conf ~/.config
+sudo cp -rpf ~/dotfiles/pacman.conf /etc
 
 echo "Press Mod + Shift + c to refresh i3 config"
