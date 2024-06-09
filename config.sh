d7d9a98d9acbbce169230ed5eaece6783c54ae1b
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-06-09 18:41:23 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -6,49 +6,39 @@ echo "Do you have paru installed? "
 read YN
 if [ $YN == "no" ]; then
   git clone "https://aur.archlinux.org/paru.git"
-  sudo chown $USER:$USER -R ~
+  sudo chown $USER:$USER -R $HOME
   cd paru 
   makepkg -sci
 fi
 
 paru -S amd-ucode alacritty reflector rofi curl nitrogen flameshot zip unzip mpv btop vim neovim picom wireplumber dunst xarchiver eza thunar thunar-archive-plugin fish make obsidian man-db xdotool vesktop-bin mercury-browser-bin neofetch gvfs polkit-gnome lxappearance bottom fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-cn-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-jp-fonts
 
-echo "---- Making backup at ~/configBackup -----"
-sudo cp -rpf ~/.config ~/configBackup 
-echo "----- Backup made at ~/configBackup ------"
+echo "---- Making backup at $HOME/configBackup -----"
+sudo cp -rpf $HOME/.config $HOME/configBackup 
+echo "----- Backup made at $HOME/configBackup ------"
 
-sudo cp -rpf ~/dotfiles/dunst ~/.config
-sudo cp -rpf ~/dotfiles/alacritty ~/.config
-sudo cp -rpf ~/dotfiles/nitrogen ~/.config
-sudo cp -rpf ~/dotfiles/fcitx5 ~/.config
-sudo cp -rpf ~/dotfiles/mozc ~/.config
-sudo cp -rpf ~/dotfiles/fonts/SF-Mono-Powerline ~/.local/share/fonts
-sudo cp -rpf ~/dotfiles/fonts/MartianMono ~/.local/share/fonts
-sudo cp -rpf ~/dotfiles/fonts/fontconfig ~/.config
-sudo cp -rpf ~/dotfiles/fish ~/.config
-sudo cp -rpf ~/dotfiles/omf ~/.config
-sudo cp -rpf ~/dotfiles/i3 ~/.config
-sudo cp -rpf ~/dotfiles/nvim ~/.config
-sudo cp -rpf ~/dotfiles/rofi ~/.config
-sudo cp -rpf ~/dotfiles/Misc/picom.conf ~/.config
-sudo cp -rpf ~/dotfiles/Misc/pacman.conf /etc
+sudo cp -rpf $HOME/dotfiles/dunst $HOME/.config
+sudo cp -rpf $HOME/dotfiles/alacritty $HOME/.config
+sudo cp -rpf $HOME/dotfiles/nitrogen $HOME/.config
+sudo cp -rpf $HOME/dotfiles/fcitx5 $HOME/.config
+sudo cp -rpf $HOME/dotfiles/mozc $HOME/.config
+sudo cp -rpf $HOME/dotfiles/fonts/SF-Mono-Powerline $HOME/.local/share/fonts
+sudo cp -rpf $HOME/dotfiles/fonts/MartianMono $HOME/.local/share/fonts
+sudo cp -rpf $HOME/dotfiles/fonts/fontconfig $HOME/.config
+sudo cp -rpf $HOME/dotfiles/fish $HOME/.config
+sudo cp -rpf $HOME/dotfiles/omf $HOME/.config
+sudo cp -rpf $HOME/dotfiles/i3 $HOME/.config
+sudo cp -rpf $HOME/dotfiles/nvim $HOME/.config
+sudo cp -rpf $HOME/dotfiles/rofi $HOME/.config
+sudo cp -rpf $HOME/dotfiles/Misc/picom.conf $HOME/.config
+sudo cp -rpf $HOME/dotfiles/Misc/pacman.conf /etc
 
-if [[ -d ~/Pictures ]]; then
-    sudo cp -f Pictures/bgpic.jpg ~/Pictures
-else
-    sudo cp -rpf Pictures ~
-fi
+sudo mv -f $HOME/dotfiles/Pictures $HOME
+sudo mv -f $HOME/dotfiles/Videos $HOME
 
-if [[ -d ~/Videos ]]; then
-    echo "Videos dir exists"
-else
-    sudo cp -rpf Videos ~
-    sudo rm -rf ~/Videos/
-fi
+paru -S base base-devel efibootmgr sof-firmware mesa lib32-mesa linux-lts linux-lts-headers linux-zen linux-zen-headers linux-firmware
 
-paru -S base base-devel efibootmgr sof-firmware mesa lib32-mesa vulkan-nouveau lib32-vulkan-nouveau lib32-libdrm libdrm linux-lts linux-lts-headers linux-zen linux-zen-headers linux-firmware
-
-sudo cp -rpf ~/dotfiles/Misc/mkinitcpio.conf /etc/
+sudo cp -rpf $HOME/dotfiles/Misc/mkinitcpio.conf /etc/
 sudo mkinitcpio -P
 
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
