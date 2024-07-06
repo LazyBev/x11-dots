95d2aa85fccec4627dddac21dedcb989fbbc98c0
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-07-06 00:09:50 +0000

    Update UpdateConfig.sh

--- UpdateConfig.sh
+++ UpdateConfig.sh
@@ -7,7 +7,6 @@ echo "----- Backup made at $HOME/configBackup ------"
 sudo cp -rpf $HOME/dotfiles/neofetch/bk $HOME/.config/neofetch
 sudo cp -rpf $HOME/dotfiles/dunst $HOME/.config
 sudo cp -rpf $HOME/dotfiles/Pictures/bgpic.jpg ../Pictures
-sudo cp -rpf $HOME/dotfiles/nitrogen $HOME/.config
 sudo cp -rpf $HOME/dotfiles/fcitx5 $HOME/.config
 sudo cp -rpf $HOME/dotfiles/ mozc $HOME/.config
 sudo cp -rpf $HOME/dotfiles/fonts/fontconfig $HOME/.config
