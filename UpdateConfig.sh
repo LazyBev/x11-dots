2644667adf00874ef7ae51f3e0159ae5713c173f
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-07-18 20:37:50 +0000

    Update UpdateConfig.sh

--- UpdateConfig.sh
+++ UpdateConfig.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 echo "---- Making backup at $HOME/configBackup -----"
-sudo mv $HOME/.config $HOME/configBackup 
+sudo cp -rpf $HOME/.config $HOME/configBackup 
 echo "----- Backup made at $HOME/configBackup ------"
 
-sudo mv $HOME/dotfiles/.emacs.d $HOME
-sudo mv $HOME/dotfiles/neofetch/bk $HOME/.config/neofetch
-sudo mv $HOME/dotfiles/dunst $HOME/.config
-sudo mv $HOME/dotfiles/Pictures/bgpic.jpg ../Pictures
-sudo mv $HOME/dotfiles/fcitx5 $HOME/.config
-sudo mv $HOME/dotfiles/mozc $HOME/.config
-sudo mv $HOME/dotfiles/fonts/fontconfig $HOME/.config
-sudo mv $HOME/dotfiles/fonts/MartianMono $HOME/.local/share/fonts
-sudo mv $HOME/dotfiles/fonts/SF-Mono-Powerline $HOME/.local/share/fonts
-sudo mv $HOME/dotfiles/fish $HOME/.config
-sudo mv $HOME/dotfiles/i3 $HOME/.config
-sudo mv $HOME/dotfiles/nvim $HOME/.config
-sudo mv $HOME/dotfiles/rofi $HOME/.config
-sudo mv $HOME/dotfiles/omf $HOME/.config
-sudo mv $HOME/dotfiles/Misc/picom.conf $HOME/.config
-sudo mv $HOME/dotfiles/Misc/pacman.conf /etc
+sudo cp -rpf $HOME/dotfiles/.emacs.d $HOME
+sudo cp -rpf $HOME/dotfiles/neofetch/bk $HOME/.config/neofetch
+sudo cp -rpf $HOME/dotfiles/dunst $HOME/.config
+sudo cp -rpf $HOME/dotfiles/Pictures/bgpic.jpg ../Pictures
+sudo cp -rpf $HOME/dotfiles/fcitx5 $HOME/.config
+sudo cp -rpf $HOME/dotfiles/mozc $HOME/.config
+sudo cp -rpf $HOME/dotfiles/fonts/fontconfig $HOME/.config
+sudo cp -rpf $HOME/dotfiles/fonts/MartianMono $HOME/.local/share/fonts
+sudo cp -rpf $HOME/dotfiles/fonts/SF-Mono-Powerline $HOME/.local/share/fonts
+sudo cp -rpf $HOME/dotfiles/fish $HOME/.config
+sudo cp -rpf $HOME/dotfiles/i3 $HOME/.config
+sudo cp -rpf $HOME/dotfiles/nvim $HOME/.config
+sudo cp -rpf $HOME/dotfiles/rofi $HOME/.config
+sudo cp -rpf $HOME/dotfiles/omf $HOME/.config
+sudo cp -rpf $HOME/dotfiles/Misc/picom.conf $HOME/.config
+sudo cp -rpf $HOME/dotfiles/Misc/pacman.conf /etc
 
 echo "Press Mod + Shift + c to refresh i3 config"
