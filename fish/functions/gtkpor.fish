8177bef736a49702f65c25642d29e05a897bdfb7
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-08-20 02:05:28 +0000

    Add files via upload

---
+++ fish/functions/gtkpor.fish
@@ -0,0 +1,4 @@
+function gtkpor --wraps='/usr/lib/xdg-desktop-portal --replace' --description 'alias gtkpor=/usr/lib/xdg-desktop-portal --replace'
+  /usr/lib/xdg-desktop-portal --replace $argv
+        
+end
