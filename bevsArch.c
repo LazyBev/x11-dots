e15b9f05362d51619502670c50c0e17bb5fe9d05
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-10-22 20:28:50 +0000

    Update bevsArch.c

--- bevsArch.c
+++ bevsArch.c
@@ -1,138 +1,114 @@
-#include <stdio.h> 
-#include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
+#!/bin/bash
 
-// Initializing variables
-char root[100], efi[100], user[100], pass[100], tpass[100], y[10], laut[100];
-void disk();
-void prof();
-void arch();
-void chroot();
+# Initialize variables
+efi=""
+root=""
+user=""
+pass=""
+tpass=""
+laut=""
 
-void disk() {
-    // Partition disks
-    system("lsblk");
-    printf("Please enter EFI partition: (example /dev/sda1 or /dev/nvme0n1p1): ");
-    scanf("%s", efi);
-    printf("Please enter ROOT partition: (example /dev/sda2 or /dev/nvme0n1p2): ");
-    scanf("%s", root);
+disk() {
+    # Partition disks
+    lsblk
+    read -p "Please enter EFI partition (example /dev/sda1 or /dev/nvme0n1p1): " efi
+    read -p "Please enter ROOT partition (example /dev/sda2 or /dev/nvme0n1p2): " root
 
-    // Make the filesystems and mounting to targets
-    char command[256];
-    printf("\nCreating Filesystems...\n");
-    snprintf(command, sizeof(command), "mkfs.fat -F 32 %s", efi);
-    system(command);
-    snprintf(command, sizeof(command), "mkfs.ext4 %s", root);
-    system(command);
-    system("mkdir -p /mnt/boot");
-    snprintf(command, sizeof(command), "mount %s /mnt/boot", efi);
-    system(command);
-    snprintf(command, sizeof(command), "mount %s /mnt", root);
-    system(command);
+    # Make the filesystems and mount to targets
+    echo -e "\nCreating Filesystems..."
+    mkfs.fat -F 32 "$efi"
+    mkfs.ext4 "$root"
+    mkdir -p /mnt/boot
+    mount "$efi" /mnt/boot
+    mount "$root" /mnt
 }
 
-void prof() {
-    printf("Please enter your username: ");
-    scanf("%s", user); 
+prof() {
+    read -p "Please enter your username: " user
 
-    do {
-        printf("Please enter your password: ");
-        scanf("%s", pass); 
+    while true; do
+        read -sp "Please enter your password: " pass
+        echo
+        read -sp "Please enter your password again: " tpass
+        echo
 
-        printf("Please enter your password again: ");
-        scanf("%s", tpass);
-
-        if (strcmp(tpass, pass) == 0) {
-            printf("Passwords match\n");
-        } else {
-            printf("Passwords do not match, please try again\n");
-        }
-
-    } while(strcmp(tpass, pass) != 0);
+        if [ "$pass" == "$tpass" ]; then
+            echo "Passwords match"
+            break
+        else
+            echo "Passwords do not match, please try again"
+        fi
+    done
 }
 
-void arch() {
-    system("sudo cp -rpf Misc/pacman.conf /mnt/etc");
-    system("pacstrap -K /mnt amd_ucode kitty systemd base xdg-desktop-portal xdg-desktop-portal-gtk base-devel efibootmgr sof-firmware mesa lib32-mesa systemd linux-lts linux-lts-headers linux-zen linux-zen-headers linux-firmware networkmanager network-manager-applet wireless_tools neofetch gvfs pavucontrol polkit-gnome lxappearance bottom fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-han-serif-kr-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts nano steam wine git rofi curl alacritty make cmake meson obsidian man-db xdotool thuanr reflector nitrogen flameshot zip unzip mpv btop emacs noto-fonts picom wireplumber dunst xarchiver eza thunar-archive-plugin fish --noconfirm --needed");
-    system("genfstab -U /mnt >> /mnt/etc/fstab");
-    system("cd ..");
-    system("sudo mv -f dotfiles /mnt");
+arch() {
+    sudo cp -rpf Misc/pacman.conf /mnt/etc
+    pacstrap -K /mnt amd_ucode kitty systemd base xdg-desktop-portal xdg-desktop-portal-gtk base-devel efibootmgr sof-firmware mesa lib32-mesa systemd linux-lts linux-lts-headers linux-zen linux-zen-headers linux-firmware networkmanager network-manager-applet wireless_tools neofetch gvfs pavucontrol polkit-gnome lxappearance bottom fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-han-serif-kr-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts nano steam wine git rofi curl alacritty make cmake meson obsidian man-db xdotool thuanr reflector nitrogen flameshot zip unzip mpv btop emacs noto-fonts picom wireplumber dunst xarchiver eza thunar-archive-plugin fish --noconfirm --needed
+    genfstab -U /mnt >> /mnt/etc/fstab
+    cd ..
+    sudo mv -f dotfiles /mnt
 }
 
-void chroot() {
-    char command[256];
-    snprintf(command, sizeof(command), "arch-chroot /mnt");
-    system(command);
+chroot_system() {
+    arch-chroot /mnt <<EOF
+    useradd -m $user
+    usermod -aG wheel,storage,power,audio $user
+    echo "$user:$pass" | chpasswd
+    sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+    mv -f /dotfiles /home/$user && cd /home/$user
 
-    snprintf(command, sizeof(command), "useradd -m %s", user);
-    system(command);
+    echo "Which keyboard layout would you like?"
+    read -r laut
+    loadkeys "$laut"
 
-    snprintf(command, sizeof(command), "usermod -aG wheel,storage,power,audio %s", user);
-    system(command);
+    # Setup Language to US and set locale
+    sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+    locale-gen
+    echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
-    snprintf(command, sizeof(command), "echo %s:%s | chpasswd", user, pass);
-    system(command);
+    ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime
+    hwclock --systohc
 
-    system("sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers");
+    echo "gentuwu" > /etc/hostname
 
-    snprintf(command, sizeof(command), "sudo mv -f dotfiles /home/%s && cd /home/%s", user, user);
-    system(command);
+    # Display and Audio Drivers
+    pacman -Syu xorg xorg-server pipewire-pulse pipewire --noconfirm --needed
 
-    printf("Which layout would you like?: ");
-    scanf("%s", laut);
-    snprintf(command, sizeof(command), "loadkeys %s", laut);
-    system(command);
+    systemctl enable NetworkManager
+    /usr/lib/xdg-desktop-portal --replace
 
-    // Setup Language to US and set locale
-    system("sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen");
-    system("locale-gen");
-    system("echo \"LANG=en_US.UTF-8\" >> /etc/locale.conf");
+    # Desktop environment
+    pacman -S i3 --noconfirm --needed
 
-    system("ln -sf /usr/share/zoneinfo/Europe/London /etc/localtime");
-    system("hwclock --systohc");
+    # Packages
+    git clone "https://aur.archlinux.org/paru.git"
+    chown $user:$user -R paru
+    cd paru && makepkg -sci
 
-    system("echo \"gentuwu\" > /etc/hostname");
+    paru -S vesktop-bin mercury-browser-bin
 
-    // Display and Audio Drivers
-    system("pacman -Syu xorg xorg-server pipewire-pulse pipewire --noconfirm --needed");
+    # Mirrors
+    cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
+    reflector --verbose --latest 5 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 
-    system("systemctl enable NetworkManager");
-    system("/usr/lib/xdg-desktop-portal --replace");
-    
-    // Desktop environment
-    system("pacman -S i3 --noconfirm --needed");
-    
-    // Packages
-    system("git clone \"https://aur.archlinux.org/paru.git\"");
-    snprintf(command, sizeof(command), "sudo chown %s:%s -R paru", user, user);
-    system(command);
-    system("cd paru && makepkg -sci");
+    # My config
+    ./UpdateConfig.sh
 
-    system("paru -S vesktop-bin mercury-browser-bin");
+    mkinitcpio -P
 
-    // Mirrors
-    system("sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak");
-    system("sudo reflector --verbose --latest 5 --protocol https --sort rate --save /etc/pacman.d/mirrorlist");
+    git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
+    ~/.config/emacs/bin/doom install
+    ~/.config/emacs/bin/doom sync
 
-    // My config
-    snprintf(command, sizeof(command), "./UpdateConfig.sh");
-    system(command);
-
-    system("sudo mkinitcpio -P");
-    
-    system("git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs");
-    system("~/.config/emacs/bin/doom install");
-    system("~/.config/emacs/bin/doom sync");
-    
-    system("curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish");
+    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+EOF
 }
 
-int main(void) {
-    disk();
-    prof();
-    arch();
-    chroot();
-
-    return 0;
+main() {
+    disk
+    prof
+    arch
+    chroot_system
 }
+
+main
