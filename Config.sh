9c7d6ca8ea7799b52086466b5f2fdd1cf9f1e918
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-10-25 20:56:04 +0000

    Update Config.sh

--- Config.sh
+++ Config.sh
@@ -7,34 +7,35 @@ main() {
 
     # Packages
     git clone https://aur.archlinux.org/yay-bin.git
-    chown "$user:$user" -R yay-bin && cd yay-bin 
+    sudo chown "$user:$user" -R yay-bin && cd yay-bin 
     makepkg -si && cd ..
 
-    yay -S firefox nvidia-dkms nvidia-utils nmap wireshark-qt neovim vim john hydra aircrack-ng sqlmap hashcat nikto openbsd-netcat metasploit amd_ucode kitty systemd base xdg-desktop-portal xdg-desktop-portal-gtk base-devel efibootmgr sof-firmware mesa lib32-mesa vulkan-mesa-layers lib32-vulkan-mesa-layers systemd linux linux-headers linux-firmware networkmanager network-manager-applet wireless_tools neofetch gvfs pavucontrol polkit-gnome lxappearance bottom fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-han-serif-kr-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts nano steam wine git rofi curl make cmake meson obsidian man-db xdotool thunar nitrogen flameshot zip unzip mpv btop emacs noto-fonts picom wireplumber dunst xarchiver eza thunar-archive-plugin fish
+    yay -Syu firefox nvidia-dkms nvidia-utils nmap wireshark-qt neovim vim john hydra aircrack-ng sqlmap hashcat nikto openbsd-netcat metasploit amd_ucode kitty systemd base xdg-desktop-portal xdg-desktop-portal-gtk base-devel efibootmgr sof-firmware mesa lib32-mesa vulkan-mesa-layers lib32-vulkan-mesa-layers systemd linux linux-headers linux-firmware networkmanager network-manager-applet wireless_tools neofetch gvfs pavucontrol polkit-gnome lxappearance bottom fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-han-serif-kr-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts nano steam wine rofi curl make cmake meson obsidian man-db xdotool thunar nitrogen flameshot zip unzip mpv btop emacs noto-fonts picom wireplumber dunst xarchiver eza thunar-archive-plugin fish
 
     # My config
     echo "---- Making backup at $HOME/configBackup -----"
     sudo cp -rpf "$HOME/.config" "$HOME/configBackup"
     echo "----- Backup made at $HOME/configBackup ------"
+    echo "Update your 13 config by hitting Mod+Shift+c"
 
     # Copy configurations
-    for config in .emacs.d dunst neofetch/bk fcitx5 mozc fish i3 nvim rofi omf Misc/picom.conf; do
-        sudo cp -rpf "$HOME/dotfiles/$config" "$HOME/.config/$config"
+    for config in dunst fcitx5 mozc fish i3 nvim rofi omf; do
+        sudo cp -rpf "$HOME/dotfiles/$config" "$HOME/.config/";
     done
 
     for fonts in fonts/MartianMono fonts/SF-Mono-Powerline fonts/fontconfig; do
-        sudo cp -rpf "$HOME/dotfiles/$fonts" "$HOME/.local/share/fonts/"
+        sudo cp -rpf "$HOME/dotfiles/$fonts" "$HOME/.local/share/fonts/";
     done
-    
-    sudo cp -rpf "$HOME/dotfiles/Pictures/bgpic.jpg" "$HOME/Pictures/"
-    sudo cp -rpf "$HOME/dotfiles/Misc/pacman.conf"
 
-    git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.config/emacs
+    mkdir -p "$HOME/.config/neofetch/" && sudo cp --parents -rpf "$HOME/dotfiles/neofetch/bk" "$HOME/.config/neofetch/"
+    mkdir -p "$HOME/Pictures/" && sudo cp -rpf "$HOME/dotfiles/Pictures/bgpic.jpg" "$HOME/Pictures/"
+    sudo cp -rpf "$HOME/dotfiles/Misc/pacman.conf" "$HOME/.config/"
+
     ~/.config/emacs/bin/doom install
     ~/.config/emacs/bin/doom sync
 
     curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-    chsh -s fish
+    chsh -s /usr/bin/fish
     reboot
 }
 
