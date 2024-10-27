63e4e92b6724f162c516789b186a6e2527a04845
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-10-27 13:51:17 +0000

    Update ScriptSelect.sh

--- ScriptSelect.sh
+++ ScriptSelect.sh
@@ -8,7 +8,36 @@ echo -n "4) Gaming Script"
 echo -n "5) All"
 read -p ": " scr
 
-case $scr in
+cd Scripts
 
+case "$scr" in
   1)
-    
+    sudo chmod +x ./Config.sh
+    ./Config.sh
+    ;;
+  
+  2)
+    sudo chmod +x ./UpdateConfig.sh
+    ./UpdateConfig.sh
+    ;;
+  
+  3)
+    sudo chmod +x ./Nvidia.sh
+    ./Nvidia.sh
+    ;;
+
+  4)
+    sudo chmod +x ./Gaming.sh
+    ./Gaming.sh
+    ;;
+  
+  5)
+    for script in Config UpdateConfig Nvidia Gaming; do
+        sudo chmod +x ./"$script".sh && ./"$script".sh
+    done
+    ;;
+  
+  *)
+    echo "Invalid option."
+    ;;
+esac
