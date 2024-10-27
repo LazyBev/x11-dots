e6bda277d5546d5ef7ca7cbddbef2acd7da0cc6c
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-10-27 13:31:29 +0000

    Create ScriptSelect.sh

---
+++ ScriptSelect.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo -n "Choose a script to run."
+echo -n "1) Install My Config"
+echo -n "2) Update Installed Config"
+echo -n "3) Nvidia GPU Script"
+echo -n "4) Gaming Script"
+echo -n "5) All"
+read -p ": " scr
+
+case $scr in
+
+  1)
+    
