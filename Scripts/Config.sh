2237b085e49be3b7b47fb063bdf9dcc8df185b60
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-10-27 13:43:42 +0000

    Update Config.sh

--- Scripts/Config.sh
+++ Scripts/Config.sh
@@ -4,41 +4,39 @@ set -e
 set -a
 set -u
 
-main() {
-    user=$(whoami)
+user=$(whoami)
 
-    # Packages
-    git clone https://aur.archlinux.org/yay-bin.git
-    sudo chown "$user:$user" -R yay-bin && cd yay-bin 
-    makepkg -si && cd ..
+# Packages
+git clone https://aur.archlinux.org/yay-bin.git
+sudo chown "$user:$user" -R yay-bin && cd yay-bin 
+makepkg -si && cd ..
 
-    yay -Syu firefox flatpak nvidia-dkms nvidia-utils nmap wireshark-qt neovim vim john hydra aircrack-ng sqlmap hashcat nikto openbsd-netcat metasploit amd_ucode kitty systemd base xdg-desktop-portal xdg-desktop-portal-gtk base-devel efibootmgr sof-firmware mesa lib32-mesa vulkan-mesa-layers lib32-vulkan-mesa-layers systemd linux linux-headers linux-firmware networkmanager network-manager-applet wireless_tools neofetch gvfs pavucontrol polkit-gnome lxappearance bottom fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-han-serif-kr-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts nano rofi curl make cmake meson obsidian man-db xdotool thunar nitrogen flameshot zip unzip mpv btop emacs noto-fonts picom wireplumber dunst xarchiver eza thunar-archive-plugin
+yay -Syu firefox flatpak nvidia-dkms nvidia-utils nmap wireshark-qt neovim vim john hydra aircrack-ng sqlmap hashcat nikto openbsd-netcat metasploit amd_ucode kitty systemd base xdg-desktop-portal xdg-desktop-portal-gtk base-devel efibootmgr sof-firmware mesa lib32-mesa vulkan-mesa-layers lib32-vulkan-mesa-layers systemd linux linux-headers linux-firmware networkmanager network-manager-applet wireless_tools neofetch gvfs pavucontrol polkit-gnome lxappearance bottom fcitx5-im fcitx5-mozc adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts adobe-source-han-sans-kr-fonts adobe-source-han-serif-kr-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts nano rofi curl make cmake meson obsidian man-db xdotool thunar nitrogen flameshot zip unzip mpv btop emacs noto-fonts picom wireplumber dunst xarchiver eza thunar-archive-plugin
 
-    # My config
-    echo "---- Making backup at $HOME/configBackup -----"
-    sudo cp -rpf "$HOME/.config" "$HOME/configBackup"
-    echo "----- Backup made at $HOME/configBackup ------"
+# My config
+echo "---- Making backup at $HOME/configBackup -----"
+sudo cp -rpf "$HOME/.config" "$HOME/configBackup"
+echo "----- Backup made at $HOME/configBackup ------"
 
-    # Copy configurations
-    for config in dunst fcitx5 mozc fish i3 nvim rofi omf; do
-        sudo cp -rpf "$HOME/dotfiles/$config" "$HOME/.config/";
-    done
+# Copy configurations
+for config in dunst fcitx5 mozc fish i3 nvim rofi omf; do
+    sudo cp -rpf "$HOME/dotfiles/$config" "$HOME/.config/";
+done
 
-    for fonts in fonts/MartianMono fonts/SF-Mono-Powerline fonts/fontconfig; do
-        sudo cp -rpf "$HOME/dotfiles/$fonts" "$HOME/.local/share/fonts/";
-    done
+for fonts in fonts/MartianMono fonts/SF-Mono-Powerline fonts/fontconfig; do
+    sudo cp -rpf "$HOME/dotfiles/$fonts" "$HOME/.local/share/fonts/";
+done
 
-    mkdir -p "$HOME/.config/neofetch/" && sudo cp --parents -rpf "$HOME/dotfiles/neofetch/bk" "$HOME/.config/neofetch/"
-    mkdir -p "$HOME/Pictures/" && sudo cp -rpf "$HOME/dotfiles/Pictures/bgpic.jpg" "$HOME/Pictures/"
-    sudo cp -rpf "$HOME/dotfiles/Misc/pacman.conf" "$HOME/.config/"
+mkdir -p "$HOME/.config/neofetch/" && sudo cp --parents -rpf "$HOME/dotfiles/neofetch/bk" "$HOME/.config/neofetch/"
+mkdir -p "$HOME/Pictures/" && sudo cp -rpf "$HOME/dotfiles/Pictures/bgpic.jpg" "$HOME/Pictures/"
+sudo cp -rpf "$HOME/dotfiles/Misc/pacman.conf" "$HOME/.config/"
 
-    ~/.config/emacs/bin/doom install
-    ~/.config/emacs/bin/doom sync
+~/.config/emacs/bin/doom install
+~/.config/emacs/bin/doom sync
     
-    yay -S fish
+yay -S fish
 
-    curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
-    chsh -s /usr/bin/fish
-}
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+chsh -s /usr/bin/fish
 
-main
+echo "Please Reboot"
