29c1a237d5e0ebd2d47a1bf9c3e23a73afee2bff
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-10-29 04:12:25 +0000

    Update ScriptSelect.sh

--- Scripts/ScriptSelect.sh
+++ Scripts/ScriptSelect.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+set -e
+set -a
+set -u
+
 echo -n "Choose a script to run."
 echo -n "1) Install My Config"
 echo -n "2) Update Installed Config"
 echo -n "3) Nvidia GPU Script"
 echo -n "4) Gaming Script"
 echo -n "5) Bluetooth Script"
+echo -n "6) All"
 read -p ": " scr
 
 case "$scr" in
@@ -33,7 +38,12 @@ case "$scr" in
     sudo chmod +x ./Bluetooth.sh
     ./Bluetooth.sh
     ;;
-  
+    
+  6)
+    sudo chmod +x ./LazyArch.sh
+    ./LazyArch.sh
+    ;;
+    
   *)
     echo "Invalid option."
     ;;
