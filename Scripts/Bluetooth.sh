49a6778eff67622462bc5626e821cb0638af5766
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-10-29 03:47:35 +0000

    Create Bluetooth.sh

---
+++ Scripts/Bluetooth.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+
+sudo pacman -S bluez bluez-utils
+sudo systemctl enable bluetooth.service
+sudo systemctl start bluetooth.service
+lsusb | grep -i bluetooth
+sudo systemctl daemon-reload
+
+reboot
