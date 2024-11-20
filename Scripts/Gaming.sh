6eb8de721273702a6091e1d63ce90bc144721cdd
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-11-20 00:27:19 +0000

    Update Gaming.sh

--- Scripts/Gaming.sh
+++ Scripts/Gaming.sh
@@ -1,4 +1,5 @@
 yay -S steam wine flatpak
 flatpak install --user https://sober.vinegarhq.org/sober.flatpakref
+echo "alias roblox='flatpak run org.vinegarhq.Sober'" >> ~/.bashrc
 
 echo -e "Make sure to reboot..."
