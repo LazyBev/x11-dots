cfd5aefa8a63a0f1205a27b1acbeaba19ff9c5b0
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-11-22 02:12:24 +0000

    Update Gaming.sh

--- Scripts/Gaming.sh
+++ Scripts/Gaming.sh
@@ -1,5 +1,28 @@
-yay -S steam wine flatpak
-flatpak install --user https://sober.vinegarhq.org/sober.flatpakref
+#!/bin/bash
+
+set -e
+
+yay -S steam wine flatpak winetricks
+
+read -p "Do you want to install roblox? [y/N]: " choice
+case $choice in
+    y | Y)
+        flatpak install --user https://sober.vinegarhq.org/sober.flatpakref
+        ;;
+    *)
+        echo "Roblox installation skipped."
+        ;;
+esac
+
 echo "alias roblox='flatpak run org.vinegarhq.Sober'" >> ~/.bashrc
 
-echo -e "Make sure to reboot..."
+# Prompt the user to reboot
+read -p "Would you like to reboot now? [y/N]: " reboot_choice
+case $reboot_choice in
+    y | Y)
+        run_command reboot
+        ;;
+    *)
+        echo "Reboot skipped. Please reboot manually if necessary."
+        ;;
+esac
