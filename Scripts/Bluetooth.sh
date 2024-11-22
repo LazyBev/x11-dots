3db847b653b488c6010a877b4f086ee369350df8
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-11-22 02:06:03 +0000

    Update Bluetooth.sh

--- Scripts/Bluetooth.sh
+++ Scripts/Bluetooth.sh
@@ -8,4 +8,14 @@ sudo systemctl start bluetooth.service
 sudo systemctl daemon-reload
 echo "alias blueman='blueman-manager'" >> ~/.bashrc
 source ~/.bashrc
-echo -e "Make sure to reboot..."
+
+# Prompt the user to reboot
+read -p "Would you like to reboot now? [y/N]: " reboot_choice
+case $reboot_choice in
+    y | Y)
+        run_command reboot
+        ;;
+    *)
+        echo "Reboot skipped. Please reboot manually if necessary."
+        ;;
+esac
