c7a8bdf27e3a9a2bf1db1c155109242ec885f4b0
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2025-02-10 17:26:03 +0000

    Update ArchInstallAndConfig.sh

--- Scripts/ArchInstallAndConfig.sh
+++ Scripts/ArchInstallAndConfig.sh
@@ -83,7 +83,7 @@ else
 fi
 
 # Determine disk prefix for NVMe or standard drives
-if [[ "$disk" == /dev/nvme* ]]; then
+if [[ "$disk" == /dev/nvme0n1 ]]; then
     disk_prefix="p"
 else
     disk_prefix=""
