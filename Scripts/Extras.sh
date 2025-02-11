5834d4afc04bf43bd5dc78a50ab65dcf58b51277
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2025-02-11 02:26:45 +0000

    Update Extras.sh

--- Scripts/Extras.sh
+++ Scripts/Extras.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
+set -ae
+
 read -p "Enter the hostname [gentuwu]: " hostname
 : ${hostname:=gentuwu}
-export $hostname
 
 read -p "Enter the username [user]: " user
 : ${user:=user}
-export $user
 
 read -sp "Enter the password [1234]: " password
 echo ""
 : ${password:=1234}
-export $password
 
 read -p "Enter key map for keyboard [uk]: " keyboard
 : ${keyboard:=uk}
-export $keyboard
 
 read -p "Enter the locale [en_GB.UTF-8]: " locale
 : ${locale:=en_GB.UTF-8}
-export $locale
 
 read -p "Enter the timezone [Europe/London]: " timezone
 : ${timezone:=Europe/London}
-export $timezone
 
 intel_cpu=$(hwinfo --cpu | head -n6 | grep "Intel")
 amd_cpu=$(hwinfo --cpu | head -n6 | grep "AMD")
-export cpu=""
+cpu=""
 
 if [[ -n "$intel_cpu" ]]; then
     echo "Intel CPU detected."
