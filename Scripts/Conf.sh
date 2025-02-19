7fb8cec318db1c8c33f72612a4f1e436492bc8b8
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2025-02-19 11:01:06 +0000

    Update Conf.sh

--- Scripts/Conf.sh
+++ Scripts/Conf.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 # Error handling
 trap 'echo "An error occurred. Exiting..."; exit 1;' ERR
 
-yay -Syu iwd zsh tlp stow stremio fastfetch fcitx5-im fcitx5-chinese-addons fcitx5-anthy fcitx5-hangul ttf-dejavu ttf-liberation ttf-joypixels ttf-meslo-nerd noto-fonts adobe-source-han-mono-jp-fonts adobe-source-han-mono-hk-fonts adobe-source-han-mono-kr-fonts adobe-source-han-mono-tw-fonts adobe-source-han-mono-otc-fonts adobe-source-han-mono-cn-fonts tmux blueman bluez bluez-utils steam steam-native-runtime flatpak wine winetricks neovim lua ripgrep vim librewolf-bin pulseaudio wireplumber pulseaudio-alsa alsa-utils pavucontrol ghostty i3 ranger xorg xorg-server xorg-xinit acpi git lazygit github-cli polybar xdg-desktop-portal hwinfo arch-install-scripts wireless_tools neofetch fuse2 polkit rofi curl make cmake meson obsidian man-db man-pages xdotool feh thunar wget qutebrowser flameshot zip unzip mpv btop picom dunst xarchiver eza fzf
+yay -Syu iwd zsh tlp stow stremio fastfetch cargo fcitx5-im fcitx5-chinese-addons fcitx5-anthy fcitx5-hangul ttf-dejavu ttf-liberation ttf-joypixels ttf-meslo-nerd noto-fonts adobe-source-han-mono-jp-fonts adobe-source-han-mono-hk-fonts adobe-source-han-mono-kr-fonts adobe-source-han-mono-tw-fonts adobe-source-han-mono-otc-fonts adobe-source-han-mono-cn-fonts tmux blueman bluez bluez-utils steam steam-native-runtime flatpak wine winetricks neovim lua ripgrep vim librewolf-bin pulseaudio wireplumber pulseaudio-alsa alsa-utils pavucontrol ghostty i3 ranger xorg xorg-server xorg-xinit acpi git lazygit github-cli polybar xdg-desktop-portal hwinfo arch-install-scripts wireless_tools neofetch fuse2 polkit rofi curl make cmake meson obsidian man-db man-pages xdotool feh thunar wget qutebrowser flameshot zip unzip mpv btop picom dunst xarchiver eza fzf
 wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh; sh install.sh; rm -rf install.sh;
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -12,6 +12,15 @@ git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+
+git clone https://gitlab.torproject.org/tpo/core/arti.git
+cd arti && cargo build -p arti --release;
+sudo cp target/release/arti /usr/local/bin/
+cd .. && rm -rf arti
+sudo tee ~/.config/arti/arti-config.toml <<ART
+[network]
+socks_port = 9050
+ART
 
 # Variables
 backup_dir="$HOME/configBackup_$(date +%Y%m%d_%H%M%S)"
